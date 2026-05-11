b5d4a9b4f70a15ed77bab4f4f8bea70e8dba1b4f
Kamal Kishor <120643164+Mr-Kamal2703@users.noreply.github.com>
2026-05-11 23:41:55 +0530

    Add files via upload

--- start-local.bat
+++ start-local.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-start "Mast Magan Local Server" cmd /k "node server.js"
+start "Mast Magan Local Server" powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0server.ps1"
 echo Started local server in a new window.
-echo Open http://127.0.0.1:3000/ in your browser.
+echo Open http://127.0.0.1:8000/ in your browser.
